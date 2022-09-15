1198fa7ac3afe90c4b5dfa3f6fa78b2813c31fb0
Polina Kravchenko <50082367+polinakravchenko@users.noreply.github.com>
2022-09-15 11:59:03 +0300

    Add files via upload

---
+++ test_1/js/script.js
@@ -0,0 +1,11 @@
+const N = 7; 
+const pi = 3.14;
+
+alert ("Варіант: " + N + "\nКравченко Поліна");
+
+function FunctionNumFirst() {
+    let rad = 2 * N;
+    let heigh = N + 4;
+    let V = pi * rad * rad * heigh;
+    alert ("Об'єм: " + V);
+}
